f75272c22c95a133ad25e64c2a98e5ea7424c728
Antony Jozic <39543158+antonyjozic@users.noreply.github.com>
2019-12-24 12:40:15 -0800

    origin

---
+++ modules/window.py
@@ -0,0 +1,11 @@
+import tkinter
+import sys
+import os
+
+
+#####################################################################################################
+#   This is going to be where we write all the functions involving windows, buttons within it, etc  #
+#   Also stuff like getting the file path to the input .txt or .csv, and path to an output file     #
+#####################################################################################################
+
+
