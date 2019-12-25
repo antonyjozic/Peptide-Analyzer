084737ecb1ae571f4d84a73ef8f2a1eed6825467
Antony Jozic <39543158+antonyjozic@users.noreply.github.com>
2019-12-25 14:14:09 -0800

    gets path correctly, need to error handle for proper files

--- modules/window.py
+++ modules/window.py
@@ -15,9 +15,9 @@ import os
 window = tk.Tk()
 
 window.title('Peptide-Analysis Tool')
-window.geometry('800x600')
+window.geometry('600x560')
 
-inp_file = Label(window, text="Path to .txt file containing sequences:")
+inp_file = Label(window, text="Path to .txt file containing sequences: ")
 inp_file.grid(row=0, column=0, sticky=W)
 
 inp_path_box = Entry(window, width=30)
@@ -29,8 +29,25 @@ def get_inp_path():
         inp_path_box.delete(0,END)
     inp_path_box.insert(END,fname)
 
-inp_path_button = Button(window,text='Click to select file',command=get_inp_path(), width=30)
+inp_path_button = Button(window,text='Click to select file',command=get_inp_path, width=30)
 inp_path_button.grid(row=0,column=2)
+
+out_dir = Label(window, text="Path to output directory: ")
+out_dir.grid(row=1, column=0, sticky=W)
+
+out_path_box = Entry(window, width=30)
+out_path_box.grid(row=1,column=1)
+
+
+def get_out_path():
+    fname = askdirectory()
+    if out_path_box != '':
+        out_path_box.delete(0,END)
+    out_path_box.insert(END,fname)
+
+out_path_button = Button(window,text='Click to select file',command=get_out_path, width=30)
+out_path_button.grid(row=1,column=2)
+
 
 #         #
 # TESTING #
