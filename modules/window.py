b7b7258e7479bc246488ec71eb35dac4479328f1
Antony Jozic <39543158+antonyjozic@users.noreply.github.com>
2019-12-24 20:29:37 -0800

    window opens, need to work on file path button

--- modules/window.py
+++ modules/window.py
@@ -1,4 +1,8 @@
-import tkinter
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox, scrolledtext
+from tkinter.filedialog import askopenfilename, askdirectory
+
 import sys
 import os
 
@@ -8,4 +12,28 @@ import os
 #   Also stuff like getting the file path to the input .txt or .csv, and path to an output file     #
 #####################################################################################################
 
+window = tk.Tk()
 
+window.title('Peptide-Analysis Tool')
+window.geometry('800x600')
+
+inp_file = Label(window, text="Path to .txt file containing sequences:")
+inp_file.grid(row=0, column=0, sticky=W)
+
+inp_path_box = Entry(window, width=30)
+inp_path_box.grid(row=0,column=1)
+
+def get_inp_path():
+    fname = askopenfilename()
+    if inp_path_box != '':
+        inp_path_box.delete(0,END)
+    inp_path_box.insert(END,fname)
+
+inp_path_button = Button(window,text='Click to select file',command=get_inp_path(), width=30)
+inp_path_button.grid(row=0,column=2)
+
+#         #
+# TESTING #
+#         #
+
+window.mainloop()
