0901ae0147cd18ebe5580baf70dcca9e01533921
Antony Jozic <39543158+antonyjozic@users.noreply.github.com>
2019-12-25 22:45:14 -0800

    a main function to test code associated with window module

---
+++ peptide-analysis.py
@@ -0,0 +1,13 @@
+
+###################################################
+#                                                 #
+#               Peptide-Analysis Tool             #
+#      Authors: Antony Jozic & Toshiki Chiba      #
+#                                                 #
+###################################################
+
+from modules.window import *
+from modules.analysis import *
+
+if __name__ == "__main__":
+    window.mainloop()
